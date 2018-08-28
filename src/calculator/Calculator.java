e2dd71efd70b9b24ad7bb9424455e6289f91943b
java_tsi08 <s.ishankulov@mail.ru>
2018-08-28 18:50:17 +0300

    Calculate 2 operations (cycle)

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -10,51 +10,21 @@ import java.sql.SQLOutput;
 class Calculator {
 
     String calculate(String[] expression) {
-        System.out.println(expression[0]);
-        System.out.println(expression[1]);
-        System.out.println(expression[2]);
-        Double a = Double.parseDouble(expression[0]);
-        Double b = Double.parseDouble(expression[2]);
-        Double result;
-        switch (expression[1]) {
-            case "+":
-                result = a + b;
-                break;
-            case "-":
-                result = a - b;
-                break;
-            case "/":
-                result = a / b;
-                break;
-            case "*":
-                result = a * b;
-                break;
-            default:
-                return "ERROR";
+        double result = Double.parseDouble(expression[0]);
+        for (int i = 1; i < expression.length; i += 2) {
+            String op = expression[i];
+            double b = Double.parseDouble(expression[i + 1]);
+            switch (op) {
+                case "+":
+                    result += b;
+                    break;
+                case "-":
+                    result -= b;
+                    break;
+                default:
+                    return "ERROR";
+            }
         }
-if (expression.length < 4)
-{
-    return String.valueOf(result);
-}
-        System.out.println(expression[3]);
-        System.out.println(expression[4]);
-        Double c = Double.parseDouble(expression[4]);
-        switch (expression[3]) {
-            case "+":
-                result = result + c;
-                break;
-            case "-":
-                result = result - c;
-                break;
-            case "/":
-                result = result / c;
-                break;
-            case "*":
-                result = result * c;
-                break;
-            default:
-                return "ERROR";
-        }
-        return String.valueOf(result);
+                    return String.valueOf(result);
     }
 }
